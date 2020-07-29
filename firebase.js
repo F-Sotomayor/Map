5cdf508c44ed3ef1eba7fe6031d1243fedd346dd
F-Sotomayor <facundosotomayor95@hotmail.com>
2020-07-29 16:39:26 -0300

    added firebase

---
+++ firebase.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBKLO0nvS0yshEtjZRan_5MauJeNsnnT5E",
+  authDomain: "mapa-js-b02df.firebaseapp.com",
+  databaseURL: "https://mapa-js-b02df.firebaseio.com",
+  projectId: "mapa-js-b02df",
+  storageBucket: "mapa-js-b02df.appspot.com",
+  messagingSenderId: "264259426226",
+  appId: "1:264259426226:web:53106c5d121c1f95b936ab",
+  measurementId: "G-LTC7PH7194",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();

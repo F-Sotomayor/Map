cd6a15f902eb558201abac55f2cfdf15a44b9a9c
F-Sotomayor <facundosotomayor95@hotmail.com>
2020-07-30 17:35:44 -0300

    update

--- dist/map.27237bf4.js
+++ dist/map.27237bf4.js
@@ -25307,7 +25307,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61798" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64610" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};

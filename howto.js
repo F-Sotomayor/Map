b7201643bbccb7e569ee34cabfc77e2b77aeb785
F-Sotomayor <facundosotomayor95@hotmail.com>
2020-07-30 22:19:40 -0300

    removed help queries up

---
+++ howto.js
@@ -0,0 +1,11 @@
+const closeModal = document.getElementById("closeModal");
+closeModal.addEventListener("click", (e) => {
+  document.getElementById("howtoModal").style.display = "none";
+});
+
+const openModal = document.getElementById("openModal");
+openModal.addEventListener("click", () => {
+  document.getElementById("howtoModal").style.display = "flex";
+});
+
+console.log(openModal);
